2beafa49a4fa2a370cc55a39633f799f835117c6
logictechjulyintake <julyintakelogictech@gmail.com>
2023-08-27 10:55:33 +0545

    tailwind css setup, usage

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,9 +3,10 @@ import './Post.css';
 
 function Post(props) {
     return <>
-        <div className='post'>
-            <h4>{props.title}</h4>
-            <pre>{props.body}</pre>
+        <div className="bg-green-100 p-10 m-5 rounded shadow-md">
+            <h4 className='uppercase font-bold'>{props.title}</h4>
+            <p className='break-all'>{props.body}</p>
+            <button className="mt-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Read More</button>
         </div>
     </>
 }
