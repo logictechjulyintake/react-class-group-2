2c05f62de78efe156dae14a52296b535f3434a5f
kandelsandesh <kandelsandesh2055@gmail.com>
2023-08-29 09:11:43 +0545

    Routs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FetchAPIEg from "./components/FetchAPIEg";
 import Home from "./components/Home";
+import MyForm from "./components/MyForm";
+import FetchAPIEgTwo from "./components/FetchAPIEgTwo";
+import MapExample from "./components/MapExample";
+import MyCard from "./components/MyCard";
 
 function App() {
   return (
@@ -11,6 +15,12 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/posts" element={<FetchAPIEg />} />
+          <Route path="/form" element={<MyForm />} />
+          <Route path="/featch" element={<FetchAPIEgTwo />} />
+          <Route path="/Map" element={<MapExample />} />
+          <Route path="/card" element={<MyCard />} />
+
+
         </Routes>
       </BrowserRouter>
 
